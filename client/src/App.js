2c1fc19350ca048d67578f78093821baaad28c7e
shelby <shelby@shelbys-air.lan>
2022-07-03 16:49:46 -0500

    Repo connnection success

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         AHHHHAKSDLFJA;SLKDJFA;LKSDFJ
-        </p>
+Test React Application        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
