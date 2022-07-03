9fb573f0358544a7f428947882b1c6bb5638d589
shelby <shelby@shelbys-air.lan>
2022-07-03 17:07:01 -0500

    clean up default code

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-Test React Application        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      React
     </div>
   );
 }
