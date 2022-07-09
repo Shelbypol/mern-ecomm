778c748a23248b06f2b78914b0ad3c00ec1fea3f
shelby <shelby@shelbys-air.lan>
2022-07-09 14:42:52 -0500

    registration redirects but firebase pw is not returning token result

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, isValidElement } from "react";
 import { auth } from "../../firebase";
-import { sendSignInLinkToEmail } from "firebase/auth";
+import { signInWithEmailLink, updatePassword } from "firebase/auth";
 import { toast } from "react-toastify";
 
 
@@ -9,13 +9,56 @@ const RegisterComplete = ( {history} ) =>{
     const [ email, setEmail ] = useState("")
     const [ password, setPassword ] = useState("")
 
-    useState(() =>{
+    useEffect(() =>{
         setEmail(window.localStorage.getItem("emailForRegistration"))
     }, []) //whenever the 2nd arg runs the first fn arg is called
 
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        if(!email || !password){
+            toast.error('Email and password is required')
+            return;
+        }
+
+        if(password.length < 6){
+            toast.error('Password must be 6 charactes long')
+            return;
+        }
+
+        try{
+            const result = await signInWithEmailLink(auth, email, window.location.href )
+            // console.log('result', result)
+
+            if(result.user.emailVerified){
+                // remove user email from local storage
+                window.localStorage.removeItem("emailForRegistration")
+                
+                // firebase will keep track of the currently logged in user by using auth.currentUser
+                let user = auth.currentUser;
+                console.log('user', user )
+
+                // updatePassword from firebase
+                await updatePassword(auth, password)
+
+                // get user id token - this will be how we communicate with the backend
+                // getIdTokenResult firebase resut to get user token
+                const idTokenResult = await user.getIdTokenResult()
+                console.log('user', user, 'idTokenResult', idTokenResult)
+                // redux store
+                
+
+                // redirect
+                // uses BrowserRouters history to push us to a different site location
+                // history.push('/')
+
+            }
+
+        }catch(error){
+            console.log(error)
+            toast.error(error.message)
+        }
     }
     
 
